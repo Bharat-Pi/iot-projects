4730dda0b16a1dfd5a0de60cc6e3545579b671a0
Deepak <deepak.ts@refillbot.in>
2023-08-02 13:11:34 +0530

    Initial Motion detection using PIR sensor using Bharath Pi

---
+++ Motion_detection_using_PIR_sensor/pir_sensor.ino
@@ -0,0 +1,34 @@
+int led = 32;                // the pin that the LED is atteched to
+int sensor = 23;              // the pin that the sensor is atteched to
+int state = HIGH;             // by default, no motion detected
+int val = 0;                 // variable to store the sensor status (value)
+
+void setup() {
+  pinMode(led, OUTPUT);      // initalize LED as an output
+  pinMode(sensor, INPUT);    // initialize sensor as an input
+  Serial.begin(115200);        // initialize serial
+}
+
+void loop(){
+  val = digitalRead(sensor);   // read sensor value
+  if (val == HIGH) {           // check if the sensor is HIGH
+    digitalWrite(led, HIGH);   // turn LED ON
+    delay(100);                // delay 100 milliseconds 
+    
+    if (state == LOW) {
+      Serial.println("Motion detected!"); 
+      state = HIGH;       // update variable state to HIGH
+    }
+  } 
+  else {
+      digitalWrite(led, LOW); // turn LED OFF
+      delay(200);             // delay 200 milliseconds 
+      
+      if (state == HIGH){
+        Serial.println("Motion stopped!");
+        state = LOW;       // update variable state to LOW
+    }
+  }
+}
+
+
