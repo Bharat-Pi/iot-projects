143f1644ba076a19bcf1ab19d563e3768e5f6c74
Snehaaa-C <167289991+Snehaaa-C@users.noreply.github.com>
2024-06-06 20:27:30 +0530

    Update BharatPi-Neoway-GPS-Tracker-Shield-Test.ino

--- BharatPi-Neoway-GPS-tracker-shield-test/BharatPi-Neoway-GPS-Tracker-Shield-Test/BharatPi-Neoway-GPS-Tracker-Shield-Test.ino
+++ BharatPi-Neoway-GPS-tracker-shield-test/BharatPi-Neoway-GPS-Tracker-Shield-Test/BharatPi-Neoway-GPS-Tracker-Shield-Test.ino
@@ -1,3 +1,21 @@
+/*************************************************************************
+
+   PROJECT NAME: Bharat Pi Neoway-GPS-Tracker-Shield-Test
+   AUTHOR: Bharat Pi
+   CREATED DATE: 22/01/2024
+   COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
+   VERSION: 0.1.0
+
+   DESCRIPTION: Neoway-GPS-Tracker-Shield-Test
+
+   REVISION HISTORY TABLE:
+   ------------------------------------------
+   Date      | Firmware Version | Comments
+   ------------------------------------------
+   22/01/2024 -    0.1.0       -    Initial release of sample script.
+
+ *************************************************************************/
+
 #include <SoftwareSerial.h>
 #include <TinyGPSPlus.h>
 
