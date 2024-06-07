dd03a8bc369e4b7dc5f2ec8ab11fdcbcd7ce18d5
Snehaaa-C <167289991+Snehaaa-C@users.noreply.github.com>
2024-06-07 11:37:58 +0530

    Update BharatPi_Blink_LED_Test.ino

--- BharatPi_Blink_LED_Test/BharatPi_Blink_LED_Test.ino
+++ BharatPi_Blink_LED_Test/BharatPi_Blink_LED_Test.ino
@@ -1,7 +1,7 @@
 /*************************************************************************
    PROJECT NAME: Bharat Pi Blink_LED_Test
    AUTHOR: Bharat Pi
-   CREATED DATE: 22/01/2024
+   CREATED DATE: 06/01/2024
    COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
    VERSION: 0.1.1
 
@@ -11,7 +11,7 @@
    ------------------------------------------
    Date      | Firmware Version | Comments
    ------------------------------------------
-   22/01/2024 -    0.1.0       -    Initial release of sample script.
+   06/01/2024 -    0.1.0       -    Initial release of sample script.
                                     (Used Arduino IDE version - 2.3.2,
                                     Used board manager - esp32 by Espressif version - 2.0.16)
                                     
