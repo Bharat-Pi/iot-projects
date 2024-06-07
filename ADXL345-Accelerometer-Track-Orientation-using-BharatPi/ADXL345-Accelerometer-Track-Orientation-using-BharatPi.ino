d144ef25c7659f875f2d3a9803fc53e0dd4469e9
Snehaaa-C <167289991+Snehaaa-C@users.noreply.github.com>
2024-06-07 11:20:53 +0530

    Update ADXL345-Accelerometer-Track-Orientation-using-BharatPi.ino

--- ADXL345-Accelerometer-Track-Orientation-using-BharatPi/ADXL345-Accelerometer-Track-Orientation-using-BharatPi.ino
+++ ADXL345-Accelerometer-Track-Orientation-using-BharatPi/ADXL345-Accelerometer-Track-Orientation-using-BharatPi.ino
@@ -1,7 +1,7 @@
 /*************************************************************************
    PROJECT NAME: Bharat Pi ADXL345-Accelerometer-Track-Orientation
    AUTHOR: Bharat Pi
-   CREATED DATE: 22/01/2024
+   CREATED DATE: 06/01/2024
    COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
    VERSION: 0.1.1
 
@@ -12,13 +12,12 @@
    ------------------------------------------
    Date      | Firmware Version | Comments
    ------------------------------------------
-   22/01/2024 -    0.1.0       -    Initial release of sample script to read the data
+   06/01/2024 -    0.1.0       -    Initial release of sample script to read the data
                                     (Used Arduino IDE version - 2.3.2,
                                     Used board manager - esp32 by Espressif version - 2.0.16)
                                     
    o6/06/2024 -    0.1.1       -    Added the Updated version of board manager and lib 
                                     (esp32 by Espressif version - 3.0.0)
-
 
  *************************************************************************/
 
