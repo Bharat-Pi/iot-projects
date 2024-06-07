363c22458bf53a753742131637dbd45ffcfa0802
Snehaaa-C <167289991+Snehaaa-C@users.noreply.github.com>
2024-06-07 13:11:33 +0530

    Update big_sound.ino

--- Big_sound_sensor_using_Bharath_pi/big_sound.ino
+++ Big_sound_sensor_using_Bharath_pi/big_sound.ino
@@ -12,6 +12,11 @@
    Date      | Firmware Version | Comments
    ------------------------------------------
    02/08/2023 -    0.1.0       -    Initial release of sample script.
+                                    (Used Arduino IDE version - 2.3.2,
+                                    Used board manager - esp32 by Espressif version - 2.0.16)
+                                    
+   o6/06/2024 -    0.1.1       -    Added the Updated version of board manager and lib 
+                                    (esp32 by Espressif version - 3.0.0)
 
  *************************************************************************/
 
